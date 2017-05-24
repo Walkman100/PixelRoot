882a2619da9faee03890d7fc4ce24c25a332d7ca
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-05-24 16:59:19 +0200

    Extended gitignore, reverted run aria2c if file already exists
    if the file is finished, it downloads a second file to *.1
    and aria2c is unlikely to not finish downloading the file anyway

--- set-up.sh
+++ set-up.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if command -v aria2c >/dev/null 2>&1; then
-    aria2c -s 999 -j 999 -x 16 -k 1M https://dl.google.com/dl/android/aosp/sailfish-n2g47o-factory-f2bc8024.zip
-else
-    if [ ! -f sailfish-n2g47o-factory-f2bc8024.zip ]; then
+if [ ! -f sailfish-n2g47o-factory-f2bc8024.zip ]; then
+    if command -v aria2c >/dev/null 2>&1; then
+        aria2c -s 999 -j 999 -x 16 -k 1M https://dl.google.com/dl/android/aosp/sailfish-n2g47o-factory-f2bc8024.zip
+    else
         wget https://dl.google.com/dl/android/aosp/sailfish-n2g47o-factory-f2bc8024.zip
     fi
 fi
